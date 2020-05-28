833c0d0f6790905111f7d017d8e12e41fb76e6c8
Marek <66010605+Marek8888@users.noreply.github.com>
2020-05-28 15:09:15 +0200

    Add files via upload

--- PythoNoid.py
+++ PythoNoid.py
@@ -1,1 +1,18 @@
 import pygame
+
+import sys
+
+class PythoNoid:
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode((800,800))
+
+    def run_game(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+if __name__ == '__main__':
+    game = PythoNoid()
+    game.run_game()
