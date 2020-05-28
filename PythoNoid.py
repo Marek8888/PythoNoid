5e7d0269ba9b626e410b5d257a3a7ee8c65e2e10
Marek8888 <marekubacki@interia.pl>
2020-05-28 15:33:26 +0200

    update

--- PythoNoid.py
+++ PythoNoid.py
@@ -8,14 +8,16 @@ from settings import Settings
 class PythoNoid:
     def __init__(self):
         pygame.init()
-        self.screen = pygame.display.set_mode((800,800))
+        # Create a subclass of Settings
+        self.settings = Settings(self)
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
 
     def run_game(self):
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-
+            self.screen.fill(self.settings.screen_color)
             pygame.display.flip()
 
 if __name__ == '__main__':
--- settings.py
+++ settings.py
@@ -2,7 +2,6 @@ import pygame
 
 class Settings:
     def __init__(self, sneaky):
-        self.screen = sneaky.screen
         self.screen_width = 800
         self.screen_height = 800
         self.screen_color = (230,230,230)
